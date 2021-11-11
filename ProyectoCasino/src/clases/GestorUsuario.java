54e49ac41d4397bfdbc82f9e7bd393bd9617f704
Javier <Javier@10.164.56.208>
2021-11-11 12:20:52 +0100

    hashmap + gestor usuario

---
+++ ProyectoCasino/src/clases/GestorUsuario.java
@@ -0,0 +1,59 @@
+package clases;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class GestorUsuario {
+
+	private static HashMap<String, Usuario> usuarios;
+
+	public GestorUsuario() {
+		
+		usuarios = new HashMap<>();
+
+	}
+
+
+	public void añadirUsuario(Usuario u) {
+		usuarios.put(u.getKey(), u.getValue());
+	}
+
+	
+
+	public void borrarUsuario(Usuario u) {
+		usuarios.remove(u.getKey());
+	}
+
+	
+
+	public Usuario buscarUsuario(String dni) {
+		return usuarios.get(dni);
+	}
+
+	
+
+	public static HashMap<String, Usuario> obtenerUsuarios() {
+		return usuarios;
+	}
+
+
+	public static ArrayList<Usuario> obtenerUsuariosOrdenados() {
+		TreeSet<Usuario> conjuntoOrdenadoDeUsuarios = new TreeSet<>();
+		for (Usuario u : usuarios.values()) {
+			conjuntoOrdenadoDeUsuarios.add(u);
+		}
+		return new ArrayList<Usuario>(conjuntoOrdenadoDeUsuarios);
+	}
+	public static int obtenerNumeroDeSocio(int dni) {
+		return 0;
+	}
+
+	public static HashMap<String, Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public static void setUsuarios(HashMap<String, Usuario> usuarios) {
+		GestorUsuario.usuarios = usuarios;
+	}
+
+}
--- ProyectoCasino/src/clases/Usuario.java
+++ ProyectoCasino/src/clases/Usuario.java
@@ -1,11 +1,18 @@
 package clases;
 
-public class Usuario implements Comparable<Usuario>{
+import java.io.Serializable;
+import java.util.Map.Entry;
+
+public class Usuario implements Serializable, Comparable<Usuario>, Entry<String, Usuario>{
+	
+	private static final long serialVersionUID = 1L;
 	private String dni;
 	private String nombre;
 	private String apellido;
 	private int edad;
 	private String gmail;
+	private int numUsuario;
+	private String nomUsuario;
 	private String contrasenia;
 	private int numerotargeta;
 	
@@ -14,15 +21,33 @@ public class Usuario implements Comparable<Usuario>{
 		super();
 	}
 	
-	public Usuario(String dni, String nombre, String apellido, int edad, String gmail, String contrasenia, int numerotargeta) {
+	public Usuario(String dni, String nombre, String apellido, int edad, String gmail, int numUsuario, String nomUsuario, String contrasenia, int numerotargeta) {
 		super(); 
 		this.dni = dni;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.edad = edad;
+		this.numUsuario = numUsuario;
+		this.nomUsuario = nomUsuario;
 		this.gmail = gmail;
 		this.contrasenia = contrasenia;
 		this.numerotargeta = numerotargeta;
+	}
+
+	public int getNumUsuario() {
+		return numUsuario;
+	}
+
+	public void setNumUsuario(int numUsuario) {
+		this.numUsuario = numUsuario;
+	}
+
+	public String getNomUsuario() {
+		return nomUsuario;
+	}
+
+	public void setNomUsuario(String nomUsuario) {
+		this.nomUsuario = nomUsuario;
 	}
 
 	public String getDni() {
@@ -94,17 +119,67 @@ public class Usuario implements Comparable<Usuario>{
 		this.numerotargeta = numerotargeta;
 	}
 
+	
+
 	@Override
 	public String toString() {
 		return "Usuario [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", gmail="
-				+ gmail + ", contrasenia=" + contrasenia + ", numerotargeta=" + numerotargeta + "]";
+				+ gmail + ", numUsuario=" + numUsuario + ", nomUsuario=" + nomUsuario + ", contrasenia=" + contrasenia
+				+ ", numerotargeta=" + numerotargeta + "]";
 	}
 
 	@Override
-	public int compareTo(Usuario o) {
-		// TODO Auto-generated method stub
-		return o.dni.compareTo(this.dni);
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+
+		if (obj instanceof Usuario) {
+			Usuario other = (Usuario) obj;
+			if (dni == null) {
+				if (other.dni != null)
+					return false;
+			} else if (!dni.equals(other.dni))
+				return false;
+			return true;
+		} else {
+			return false;
+		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		return result;
+	}
+
+	public int compareTo(Usuario elOtroUsuario) {
+
+		return numUsuario - elOtroUsuario.numUsuario;
+	}
+
+	@Override
+	public String getKey() {
+
+		return dni;
+	}
+
+	@Override
+	public Usuario getValue() {
+
+		return this;
+	}
+
+	@Override
+	public Usuario setValue(Usuario value) {
+		
+		//No usamos este método
+		return null;
+	}
+
 
 
 }
