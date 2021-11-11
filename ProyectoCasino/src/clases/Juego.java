2f64d85d7115657721cd1d8a09e69751a4fac892
Pabloherreroo <pabloherrero@opendeusto.es>
2021-11-11 19:11:39 +0100

    ventanalogin

--- ProyectoCasino/src/clases/Juego.java
+++ ProyectoCasino/src/clases/Juego.java
@@ -1,6 +1,6 @@
 package clases;
 
-public class Juego {
+public abstract class Juego {
 
 }
  
--- ProyectoCasino/src/ventana/VentanaLogin.java
+++ ProyectoCasino/src/ventana/VentanaLogin.java
@@ -1,25 +1,82 @@
 package ventana;
 
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
 public class VentanaLogin extends JFrame{
 	private JLabel nombreusuario;
 	private JLabel contrasenia;
 	private JButton btnEntrar;
 	private JButton btnSalir;
+	private JTextField textousuario;
+	private JPasswordField textocontra;
 	
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(300, 200, 975, 650);
-		setSize(500,400);
+		setSize(320,300);
 		setTitle( "Inicio Sesion" );
-		Container cp = this.getContentPane();
-		JPanel login = new JPanel();
-		cp.add(login);
+		JPanel contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		
+		
+		nombreusuario = new JLabel("Introduzca su nombre de usuario: ");
+		nombreusuario.setBounds(10, 20, 250, 25);
+		contentPane.add(nombreusuario);
+		nombreusuario.setFont(new Font("Tahoma", Font.BOLD, 12));
+	
+		textousuario = new JTextField(25);
+		textousuario.setBounds(50, 50, 200, 20);
+		contentPane.add(textousuario);
+	
+		contrasenia = new JLabel("Introduzca su contraseña: ");
+		contrasenia.setBounds(10, 80, 250, 25);
+		contentPane.add(contrasenia);
+		contrasenia.setFont(new Font("Tahoma", Font.BOLD, 12));
+	
+		textocontra = new JPasswordField(25);
+		textocontra.setBounds(50, 110, 200, 20);
+		contentPane.add(textocontra);
+	
+		btnSalir = new JButton("SALIR");
+		btnSalir.setBounds(180, 200, 100, 40);
+		contentPane.add(btnSalir);
+		btnSalir.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		btnEntrar = new JButton("ENTRAR");
+		btnEntrar.setBounds(10, 200, 100, 40);
+		contentPane.add(btnEntrar);
+		btnEntrar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		btnSalir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		
+		btnEntrar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				if (textousuario.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Escribe un nombre de usuario");
+				} else if (textocontra.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Escribe una contraseña");
+				} else {
+					dispose();
+				}
+			}
+		});
 	}
 
 }
