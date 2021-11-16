6fdb896d90d403b398a1003c700de5af5e879257
Alejandro Pelegrin <alex.pelegrin@opendeusto.es>
2021-11-16 15:25:33 +0100

    Atributos de las clases ficha y ruleta

--- ProyectoCasino/src/clases/Ficha.java
+++ ProyectoCasino/src/clases/Ficha.java
@@ -1,5 +1,30 @@
 package clases;
 
 public class Ficha {
+	
+	private int Valor;
+	
+	public Ficha() {
+		super();
+	}
+	
+	public Ficha(int Valor) {
+		super();
+		this.Valor = Valor;
+	}
+
+	public int getValor() {
+		return Valor;
+	}
+
+	public void setValor(int valor) {
+		Valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Ficha [Valor=" + Valor + "]";
+		
+	}
 
 }
--- ProyectoCasino/src/clases/Ruleta.java
+++ ProyectoCasino/src/clases/Ruleta.java
@@ -4,6 +4,8 @@ package clases;
 public class Ruleta extends Juego {
 
 	private int casillas;
+	private String color;
+	private String parImpar;
 	
 	
 
@@ -11,9 +13,11 @@ public class Ruleta extends Juego {
 		super();
 	}
 
-	public Ruleta(int casillas) {
+	public Ruleta(int casillas, String color, String parImpar) {
 		super();
 		this.casillas = casillas;
+		this.color = color;
+		this.parImpar = parImpar;
 	}
 
 	public int getCasillas() {
@@ -23,10 +27,26 @@ public class Ruleta extends Juego {
 	public void setCasillas(int casillas) {
 		this.casillas = casillas;
 	}
+	
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getParImpar() {
+		return parImpar;
+	}
+
+	public void setParImpar(String parImpar) {
+		this.parImpar = parImpar;
+	}
 
 	@Override
 	public String toString() {
-		return "Ruleta [casillas=" + casillas + ", getGiros()=" + getGiros() + ", getDineroapostado()="
+		return "Ruleta [casillas=" + casillas + ", color=" + color + ", parImpar=" + parImpar +", getGiros()=" + getGiros() + ", getDineroapostado()="
 				+ getDineroapostado() + ", getUsuario()=" + getUsuario() + ", toString()=" + super.toString()
 				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
 	}
