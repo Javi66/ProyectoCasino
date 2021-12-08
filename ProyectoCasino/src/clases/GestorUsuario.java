05ab1d739548b1233b767cda110fce24c908bc8c
ibarra18 <79510012+ibarra18@users.noreply.github.com>
2021-12-08 19:57:17 +0100

    jtable primer commit
    todavia no terminado

--- ProyectoCasino/src/clases/GestorUsuario.java
+++ ProyectoCasino/src/clases/GestorUsuario.java
@@ -1,4 +1,10 @@
 package clases;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.TreeSet;
--- ProyectoCasino/src/ventana/VentanaGestiondePagos.java
+++ ProyectoCasino/src/ventana/VentanaGestiondePagos.java
@@ -1,34 +1,48 @@
 package ventana;
 
 import java.awt.Container;
+import javax.swing.*;
 import java.awt.Dimension;
+import java.lang.invoke.LambdaConversionException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeMap;
+import java.util.Vector;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import clases.Usuario;
+import database.Db;
 
 public class VentanaGestiondePagos extends JFrame{
-
-	private void publicVentanaGestiondePagos() {
+private JTable table;
+private ArrayList<Usuario> usuarios;
+	public  VentanaGestiondePagos() {
 		JPanel panelCentral= new JPanel();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(300, 200, 975, 650);
 		setSize(320,300);
 		setTitle( "Gestion de Pagos" );
-		//Array bidimensional con los datos de la tabla
-		Object[][] data = { 
-				
-		};
+		JTable table = new JTable();
+		TreeMap<String, Usuario>usuarios = Db.obtenerMapaUsuario(null);
 		//Array String con los nombres de columna
-		String[] columnas= {"Usuario","NumCuenta","Pagos","Saldo"};
-	final JTable table= new JTable(data,columnas);
-	table.setPreferredScrollableViewportSize(new Dimension(500, 80));
+		
+		Vector<String> columnas = new Vector<String>( Arrays.asList( "Usuario", "NumCuenta", "Pagos","Saldo" ) );
+		DefaultTableModel model = new DefaultTableModel(  new Vector<Vector<Object>>(), columnas );
+		for(Usuario u:usuarios) {
+		model.addRow(new Object[] {"wqw","","",""});
+		table.setModel(model);}
 	JScrollPane scrollpane = new JScrollPane(table);
 	
-	panelCentral.add(table);
-	getContentPane().add(panelCentral
-			);
+	
+
+	getContentPane().add(table )
+			;
 	
 
 }
