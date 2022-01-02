08f8775227e157dc1c25333647e0c80746f4d7b0
Pabloherreroo <pabloherrero@opendeusto.es>
2022-01-02 14:57:50 +0100

    iconos

--- ProyectoCasino/src/clases/Fruta.java
+++
@@ -1,5 +0,0 @@
-package clases;
-
-public enum Fruta {
-	FRESA, SANDIA, PLATANO, MELOCOTON, PERA, MANZANA
-}
--- ProyectoCasino/src/ventana/VentanaTragaperras.java
+++ ProyectoCasino/src/ventana/VentanaTragaperras.java
@@ -1,11 +1,15 @@
 package ventana;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.util.ArrayList;
 
 import javax.swing.*;
+import javax.swing.border.*;
 
 public class VentanaTragaperras extends JFrame {
 	
@@ -16,6 +20,8 @@ public class VentanaTragaperras extends JFrame {
 	
 	private JLabel lblCreditos;
 	private JLabel lblResult;
+	
+	public static ArrayList<ImageIcon> iconos = new ArrayList<ImageIcon>();
 	
 	public static void main(String[] args) {
 		VentanaTragaperras frame = new VentanaTragaperras();
@@ -47,16 +53,55 @@ public class VentanaTragaperras extends JFrame {
 		setTitle("Tragaperras");
 		
 		PanelImagen p = new PanelImagen("/images/fondo.jpg");
+		p.setBorder(new EmptyBorder(5,5,5,5));
+		p.setLayout(null);
 		setContentPane(p);
 		
-		JPanel pInferior = new JPanel(new BorderLayout());
 		
 		btnMenu = new JButton("MENU");
-		btnMenu.setBounds(10, 400, 100, 75);
-		pInferior.add(btnMenu);
-		btnMenu.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnMenu.setBounds(100, 600, 150, 75);
+		btnMenu.setBackground(Color.RED);
+		btnMenu.setForeground(Color.WHITE);
+		btnMenu.setFont(new Font("Tahoma", Font.BOLD, 30));
+		getContentPane().add(btnMenu);
 		
-		getContentPane().add(pInferior, BorderLayout.SOUTH);
+		try {
+			btnPlay = new JButton();
+			btnPlay.setBounds(1000, 100, 200, 100);
+			ImageIcon play = new ImageIcon(getClass().getResource("/images/start.jpg"));
+			Icon iconoPlay = new ImageIcon(play.getImage().getScaledInstance(btnPlay.getWidth(), btnPlay.getHeight(), Image.SCALE_DEFAULT));
+			btnPlay.setIcon(iconoPlay);
+		} catch(Exception e) {
+			btnPlay = new JButton("PLAY");
+			btnPlay.setBackground(Color.GREEN);
+			btnPlay.setForeground(Color.WHITE);
+			btnPlay.setFont(new Font("Tahoma", Font.BOLD, 50));
+			btnPlay.setBounds(950, 100, 250, 125);
+		}
+		
+		getContentPane().add(btnPlay);
+		
+		ImageIcon siete = new ImageIcon("/images/7.jpg");
+		ImageIcon bar = new ImageIcon("/images/bar.jpg");
+		ImageIcon campana = new ImageIcon("/images/campana.jpg");
+		ImageIcon cerezas = new ImageIcon("/images/cerezas.jpg");
+		ImageIcon diamante = new ImageIcon("/images/diamante.jpg");
+		ImageIcon dolar = new ImageIcon("/images/dolar.jpg");
+		ImageIcon limon = new ImageIcon("/images/limon.jpg");
+		ImageIcon sandia = new ImageIcon("/images/sandia.jpg");
+		ImageIcon trebol = new ImageIcon("/images/trebol.jpg");
+		ImageIcon uva = new ImageIcon("/images/uva.jpg");
+		
+		iconos.add(siete);
+		iconos.add(bar);
+		iconos.add(campana);
+		iconos.add(cerezas);
+		iconos.add(diamante);
+		iconos.add(dolar);
+		iconos.add(limon);
+		iconos.add(sandia);
+		iconos.add(trebol);
+		iconos.add(uva);
 		
 	}
 	
