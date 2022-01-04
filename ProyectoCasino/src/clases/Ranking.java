bccd524a9680f728cb7e8522c255eb965c5a6051
ibarra18 <javier.ibarra@opendeusto.es>
2022-01-04 13:36:12 +0100

    BaseDedatosPUNTAJERANKING

--- ProyectoCasino/src/clases/Ranking.java
+++ ProyectoCasino/src/clases/Ranking.java
@@ -1,5 +1,105 @@
 package clases;
 
-public class Ranking {
+import java.io.Serializable;
+import java.util.Map.Entry;
+import java.util.Objects;
+
+public class Ranking implements Serializable, Entry<String, Ranking>{
+	private static final long serialVersionUID = 1L;
+	private Tragaperras tragaperras ;
+	private Ruleta ruleta;
+	private  String nombreusuario;
+	private String nomjuego;
+	private int numpartida=0;
+	private int puntaje=0;
+	public Ranking() {
+		super();
+		
+	}
+	public Ranking(String nomjuego,String nombreusuario,int numpartida,int puntaje ) {
+		super();
+		this.nomjuego=nomjuego;
+		this.nombreusuario=nombreusuario;
+		this.numpartida=numpartida;
+		this.puntaje=puntaje;
+	}
+	public Tragaperras getTragaperras() {
+		return tragaperras;
+	}
+	public void setTragaperras(Tragaperras tragaperras) {
+		this.tragaperras = tragaperras;
+	}
+	public Ruleta getRuleta() {
+		return ruleta;
+	}
+	public void setRuleta(Ruleta ruleta) {
+		this.ruleta = ruleta;
+	}
+	
+	
+	
+	@Override
+	public String getKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Ranking getValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Ranking setValue(Ranking value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getNomjuego() {
+		return nomjuego;
+	}
+	public void setNomjuego(String nomjuego) {
+		this.nomjuego = nomjuego;
+	}
+	@Override
+	public String toString() {
+		return "Ranking [tragaperras=" + tragaperras + ", ruleta=" + ruleta + ", usuario=" + nombreusuario + ", nomjuego="
+				+ nomjuego + ", numpartida=" + numpartida + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(nomjuego, numpartida, ruleta, tragaperras, nombreusuario);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ranking other = (Ranking) obj;
+		return Objects.equals(nomjuego, other.nomjuego) && numpartida == other.numpartida
+				&& Objects.equals(ruleta, other.ruleta) && Objects.equals(tragaperras, other.tragaperras)
+				&& Objects.equals(nombreusuario, other.nombreusuario);
+	}
+	public int getNumpartida() {
+		return numpartida;
+	}
+	public void setNumpartida(int numpartida) {
+		this.numpartida = numpartida;
+	}
+	public int getPuntaje() {
+		return puntaje;
+	}
+	public void setPuntaje(int puntaje) {
+		this.puntaje = puntaje;
+	}
+	public String getNombreusuario() {
+		return nombreusuario;
+	}
+	public void setNombreusuario(String nombreusuario) {
+		this.nombreusuario = nombreusuario;
+	}
+	
+	
 
 }
--- ProyectoCasino/src/ventana/VentanaRanking.java
+++ ProyectoCasino/src/ventana/VentanaRanking.java
@@ -3,7 +3,11 @@ package ventana;
 import java.awt.Container;
 import javax.swing.*;
 import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
 import java.lang.invoke.LambdaConversionException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TreeMap;
@@ -32,8 +36,35 @@ private DefaultTableModel mDatos;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(300, 200, 975, 650);
 		setSize(320,300);
-		
-		Object[] columnas ={"Nombre ", "Apellido"};
+		addWindowListener( new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				if (new File("casino1.db").exists()) {
+					// Poner el parámetro a true si se quiere reiniciar la base de datos
+					try {
+						Db.initDB( "casino1.db", false );
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}  // Abrir base de datos existente
+				} else {
+					try {
+						Db.initDB( "casino1.db", true );
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}  // Crear base de datos con datos iniciales
+				}
+				MostrarUsuarios();  // Según se inicia la ventana se visualizan los productos
+			}
+				
+			
+			public void windowClosed(WindowEvent e) {
+				Db.closeBD();
+			}}
+		);
+			
+		Object[] columnas ={"Usuario ", "Puntos "};
 	    Object datos [][]= {{"Nombre 1","apellido"},{"dada","feef"}};
 		
 		JTable table = new JTable(datos,columnas);
@@ -52,5 +83,20 @@ private DefaultTableModel mDatos;
 		ventanaRanking.setVisible(true);
 		System.out.println(ventanaRanking);
 	}
+ private void mostrarUsuarios() {
+	 final long serialVersionUID = 1L;
+	 Vector<String> cabeceras = new Vector<String>( Arrays.asList( "Usuario","Puntos" ) );
+	 mDatos = new DefaultTableModel(  // Inicializa el modelo
+				new Vector<Vector<Object>>(),  // Datos de la jtable (vector de vectores) - vacíos de momento
+				cabeceras  // Cabeceras de la jtable
+					) {
+				private static final long serialVersionUID = 1L;
 
-}
+				public boolean isCellEditable(int row, int column) {
+						if(column==0)
+							return false;
+						return true;
+ }
+};
+	 
+	 usuarios = Db.getUsuarios() ;}}
