9cce4f3f13d61bb868247c86b4e1878f7418ce48
jpomb <jpomb@DESKTOP-62VJTIV>
2022-01-06 14:48:35 +0100

    junit

---
+++ ProyectoCasino/src/jUnit/Test.java
@@ -0,0 +1,18 @@
+package jUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+
+public class Test {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@org.junit.Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
