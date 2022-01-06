f555aba7748eaa173dca6d548b46739c2ce6dbc6
javier-ramos2002 <79971646+javier-ramos2002@users.noreply.github.com>
2022-01-06 15:24:10 +0100

    Test

--- ProyectoCasino/src/clases/Test.java
+++ ProyectoCasino/src/clases/Test.java
@@ -1,18 +1,26 @@
 package clases;
 
 import static org.junit.Assert.*;
+import java.sql.Connection;
+
+import java.sql.SQLException;
 
 import org.junit.AfterClass;
 
 public class Test {
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@Test
+	public void testInsertarNuevoUsuario() {
+		try {
+			Connection con = Db.initBD("Datos.db");
+			int contAntesDeInsertar = Db.contarUsuarios();
+			Db.anadirUsuario("786385023F", "A", "B", "AB", "dfghjk");
+			int contDespuesDeInsertar = Db.contarUsuarios();
+			assertTrue(contAntesDeInsertar == contDespuesDeInsertar - 1);
+			Db.closeBD(con);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
-	@org.junit.Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-
 }
