f7cacde7f4e0344f99fc9ae19a4fcfd6a0db636e
Pabloherreroo <pabloherrero@opendeusto.es>
2022-01-08 21:12:35 +0100

    cambios tragap

--- ProyectoCasino/src/ventana/VentanaTragaperras.java
+++ ProyectoCasino/src/ventana/VentanaTragaperras.java
@@ -3,6 +3,7 @@ package ventana;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
@@ -10,13 +11,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.border.*;
 
-import clases.Icono;
 import clases.Ranking;
+import clases.Tragaperras;
 import database.Db;
 
 public class VentanaTragaperras extends JFrame {
@@ -37,15 +39,11 @@ public class VentanaTragaperras extends JFrame {
 	private int saldo = 0;
 	private int puntos = 0;
 	private int numpartida;
+	private int a = 0;
+	private int b = 0;
+	private int c = 0;
 	
-	private Icono c11;
-	private Icono c12;
-	private Icono c21;
-	private Icono c22;
-	private Icono c31;
-	private Icono c32;
-	
-	public static ArrayList<ImageIcon> iconos = new ArrayList<ImageIcon>();
+	private Random r = new Random();
 	
 	public static void main(String[] args) {
 		VentanaTragaperras frame = new VentanaTragaperras("");
@@ -82,6 +80,9 @@ public class VentanaTragaperras extends JFrame {
 		p.setBorder(new EmptyBorder(5,5,5,5));
 		p.setLayout(null);
 		setContentPane(p);
+		
+		ArrayList<ImageIcon> iconos = Tragaperras.arrayIconos();
+		
 		
 		// LABELS, BOTONES Y PANEL INFO
 		btnMenu = new JButton("MENU");
@@ -120,17 +121,6 @@ public class VentanaTragaperras extends JFrame {
 		btnPlay.setBorder(bevel);
 		getContentPane().add(btnPlay);
 		
-		btnPlay.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if(saldo<=1) {
-					JOptionPane.showMessageDialog(null, "No tienes saldo", "Error",
-							JOptionPane.ERROR_MESSAGE);
-				} else {
-					saldo -= 2;
-					lblSaldo.setText("Saldo: " + saldo);
-				}
-			}
-		});
 		
 		panelInfo = new JPanel();
 		panelInfo.setBorder(bevel);
@@ -179,28 +169,14 @@ public class VentanaTragaperras extends JFrame {
 		panelInfo.add(btnAniadir);
 		getContentPane().add(panelInfo);
 		
-		//rellenamos arraylist de iconos 
-		ImageIcon siete = new ImageIcon("/images/7.jpg");
-		ImageIcon bar = new ImageIcon("/images/bar.jpg");
-		ImageIcon campana = new ImageIcon("/images/campana.jpg");
-		ImageIcon cerezas = new ImageIcon("/images/cerezas.jpg");
-		ImageIcon diamante = new ImageIcon("/images/diamante.jpg");
-		ImageIcon dolar = new ImageIcon("/images/dolar.jpg");
-		ImageIcon limon = new ImageIcon("/images/limon.jpg");
-		ImageIcon sandia = new ImageIcon("/images/sandia.jpg");
-		ImageIcon trebol = new ImageIcon("/images/trebol.jpg");
-		ImageIcon uva = new ImageIcon("/images/uva.jpg");
+		//label tragaperras
+		JLabel lblTragap1 = new JLabel();
+		JLabel lblTragap2 = new JLabel();
+		JLabel lblTragap3 = new JLabel();
 		
-		iconos.add(siete);
-		iconos.add(bar);
-		iconos.add(campana);
-		iconos.add(cerezas);
-		iconos.add(diamante);
-		iconos.add(dolar);
-		iconos.add(limon);
-		iconos.add(sandia);
-		iconos.add(trebol);
-		iconos.add(uva);
+		lblTragap1.setIcon(iconos.get(a));
+		lblTragap2.setIcon(iconos.get(b));
+		lblTragap3.setIcon(iconos.get(c));
 		
 		//creamos panel tragaperras
 		JPanel panelTragap = new JPanel();
@@ -208,7 +184,7 @@ public class VentanaTragaperras extends JFrame {
 		panelTragap.setBounds(100, 100, 690, 240);
 		panelTragap.setBackground(Color.lightGray);
 		
-		JPanel panelTragap1 = new JPanel();
+		JPanel panelTragap1 = new JPanel(new FlowLayout());
 		panelTragap1.setBorder(new BevelBorder(BevelBorder.LOWERED));
 		
 		JPanel panelTragap2 = new JPanel();
@@ -216,6 +192,10 @@ public class VentanaTragaperras extends JFrame {
 		
 		JPanel panelTragap3 = new JPanel();
 		panelTragap3.setBorder(new BevelBorder(BevelBorder.LOWERED));
+			
+		//panelTragap1.add(lblTragap1);
+		//panelTragap2.add(lblTragap2);
+		//panelTragap3.add(lblTragap3);
 		
 		//organizamos el panel de la maquina tragaperras
 		GroupLayout tragapLayout = new GroupLayout(panelTragap);
@@ -241,30 +221,79 @@ public class VentanaTragaperras extends JFrame {
 							.addComponent(panelTragap3, GroupLayout.DEFAULT_SIZE, 210, Short.MAX_VALUE))
 							.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 				);
+		
+		 GroupLayout panelTragap1L = new GroupLayout(panelTragap1);
+		 	panelTragap1.setLayout(panelTragap1L);
+		 	panelTragap1L.setHorizontalGroup(
+		 		panelTragap1L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap1L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap1)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+		 	panelTragap1L.setVerticalGroup(
+		 		panelTragap1L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap1L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap1)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+
+		    GroupLayout panelTragap2L = new GroupLayout(panelTragap2);
+		    panelTragap2.setLayout(panelTragap2L);
+		    panelTragap2L.setHorizontalGroup(
+		    	panelTragap2L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap2L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap2)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+		    panelTragap2L.setVerticalGroup(
+		    	panelTragap2L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap2L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap2)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+
+		    GroupLayout panelTragap3L = new GroupLayout(panelTragap3);
+		    panelTragap3.setLayout(panelTragap3L);
+		    panelTragap3L.setHorizontalGroup(
+		    	panelTragap3L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap3L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap3)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+		    panelTragap3L.setVerticalGroup(
+		    	panelTragap3L.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(panelTragap3L.createSequentialGroup()
+		            .addContainerGap()
+		            .addComponent(lblTragap3)
+		            .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		        );
+		
 		getContentPane().add(panelTragap);
 		
-		//Hacemos los tres carretes a partir del array de iconos
-		//CARRETE 1
-		c11 = new Icono();
-		c11.setX(115);
-		c12 = new Icono();
-		c12.setX(115);
-		c12.setY(c11.getY()-210);
-		
-		//CARRETE 2
-		c21 = new Icono();
-		c21.setX(340);
-		c22 = new Icono();
-		c22.setX(340);
-		c22.setY(c21.getY()-210);
-		
-		//CARRETE 2
-		c31 = new Icono();
-		c31.setX(565);
-		c32 = new Icono();
-		c32.setX(565);
-		c32.setY(c31.getY()-210);
-		
+		btnPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(saldo<=1) {
+					JOptionPane.showMessageDialog(null, "No tienes saldo", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					saldo -= 2;
+					lblSaldo.setText("Saldo: " + saldo);
+					a = r.nextInt(iconos.size());
+					b = r.nextInt(iconos.size());
+					c = r.nextInt(iconos.size());
+					lblTragap1.setIcon(iconos.get(a));
+					lblTragap2.setIcon(iconos.get(b));
+					lblTragap3.setIcon(iconos.get(c));
+					Tragaperras.score(puntos, a, b, c);
+					lblPuntaje.setText("Puntos: "+ puntos);
+				}
+			}
+		});
 		
 	}
 	
