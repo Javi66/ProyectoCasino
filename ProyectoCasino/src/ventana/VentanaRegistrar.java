e61782869e9b492253f1417135eebc809450ecf1
jpomb <jpomb@DESKTOP-62VJTIV>
2022-01-09 15:24:01 +0100

    ventana registrar

--- ProyectoCasino/src/ventana/VentanaRegistrar.java
+++ ProyectoCasino/src/ventana/VentanaRegistrar.java
@@ -19,6 +19,9 @@ import clases.Usuario;
 import database.Db;
 
 
+import javax.swing.GroupLayout.Alignment;
+
+
 public class VentanaRegistrar extends JFrame  {
 	private static final long  serialVersionUID = 1L;
 	private JPanel contentpane;
@@ -29,8 +32,10 @@ public class VentanaRegistrar extends JFrame  {
 	private JTextField Gmailjt;
 	private JTextField nomusuariojt;
 	private JTextField contraseniajt;
+	private JTextField numtarjetajt;
 	private JButton btnGuardar;
 	private JButton btnSalir;
+	private JProgressBar progressBar;
 	
 	
 	public VentanaRegistrar() {
@@ -41,7 +46,6 @@ public class VentanaRegistrar extends JFrame  {
 		
 		contentpane = new JPanel();
 		contentpane.setBackground(new Color(255, 255, 255));
-		contentpane.setLayout(new BorderLayout());
 	
 		//Aniadir Jlabels y Jtextfields
 		JLabel Dnijp = new JLabel("DNI:");
@@ -58,6 +62,8 @@ public class VentanaRegistrar extends JFrame  {
 		nomusuariojp.setBounds(10, 210, 80, 25);
 		JLabel contraseniajp =new JLabel("Contraseña:");
 		contraseniajp.setBounds(10, 250, 80, 25);
+		JLabel numtarjetajp =new JLabel("Tarjeta:");
+		numtarjetajp.setBounds(10, 290, 80, 25);
 		Dnijt = new JTextField(50);
 		Dnijt.setBounds(80, 10, 200, 25);
 		Nombrejt= new JTextField(30);
@@ -72,16 +78,19 @@ public class VentanaRegistrar extends JFrame  {
 		nomusuariojt.setBounds(80, 210, 200, 25);
 		contraseniajt =new JTextField(30);
 		contraseniajt.setBounds(80, 250, 200, 25);
+		numtarjetajt =new JTextField(30);
+		numtarjetajt.setBounds(80, 290, 200, 25);
 		
 		
 		//Aniadir Botones 
 		JPanel panelbotones = new JPanel();
+		panelbotones.setBounds(0, 0, 0, 0);
 		
 		
 		btnGuardar= new JButton("GUARDAR");
 		btnSalir= new JButton("SALIR");
-		btnGuardar.setBounds(10, 290, 100, 25);
-		btnSalir.setBounds(150, 290, 100, 25);
+		btnGuardar.setBounds(10, 337, 100, 25);
+		btnSalir.setBounds(150, 337, 100, 25);
 		btnGuardar.addActionListener(new ActionListener() {
 			
 			@Override
@@ -93,19 +102,33 @@ public class VentanaRegistrar extends JFrame  {
 				String apellido = Apellidojt.getText();
 				String gmail = Gmailjt.getText();
 				int edad = Integer.parseInt(Edadjt.getText());
-				Usuario u = new Usuario(dni, nombre, apellido, edad, gmail, nombreus, contrasenia, 0);
+				int numerotarjeta = Integer.parseInt(numtarjetajt.getText());
+				Usuario u = new Usuario(dni, nombre, apellido, edad, gmail, nombreus, contrasenia, numerotarjeta);
 				try {
 					Db.initDB("casino1.db", false);
 					Db.anadirUsuario(u);
-					dispose();
-					Db.closeBD();
+					Thread hilo = new Thread() {
+						@Override
+						public void run() {
+							for (int i=1;i<100;i++) {
+								progressBar.setValue(i);
+								try {Thread.sleep( 50 ); } catch (Exception e) {}
+							}
+
+							JOptionPane.showMessageDialog(null, "Se ha registrado correctamente", "Registro finalizado.",JOptionPane.INFORMATION_MESSAGE);
+							dispose();
+							Db.closeBD();
+							Main vl = new Main("");
+							vl.setVisible(true);
+						}
+					};
+					hilo.start();
+					
+								
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-				Main vl = new Main("");
-				vl.setVisible(true);
 			}
 		}); 
 		btnSalir.addActionListener(new ActionListener() {
@@ -117,8 +140,11 @@ public class VentanaRegistrar extends JFrame  {
 				vl.setVisible(true);
 			}
 		});
+		contentpane.setLayout(null);
 		
-		contentpane.setLayout(new GroupLayout(contentpane));
+		progressBar = new JProgressBar();
+		progressBar.setBounds(10, 373, 146, 9);
+		contentpane.add(progressBar);
 		
 		contentpane.add(Dnijp);
 		contentpane.add(Dnijt);
@@ -134,12 +160,35 @@ public class VentanaRegistrar extends JFrame  {
 		contentpane.add(nomusuariojt);
 		contentpane.add(contraseniajp);
 		contentpane.add(contraseniajt);
+		contentpane.add(numtarjetajp);
+		contentpane.add(numtarjetajt);
 		contentpane.add(panelbotones);
 		contentpane.add(btnGuardar);
 		contentpane.add(btnSalir);
-		this.setSize(301, 400);
+		this.setSize(301, 436);
 		
 		setContentPane(contentpane);
+		contentpane.setLayout(null);
+		contentpane.add(progressBar);
+		contentpane.add(Dnijp);
+		contentpane.add(Dnijt);
+		contentpane.add(Nombrejp);
+		contentpane.add(Nombrejt);
+		contentpane.add(Apellidojp);
+		contentpane.add(Apellidojt);
+		contentpane.add(Edadjp);
+		contentpane.add(Edadjt);
+		contentpane.add(Gmailjp);
+		contentpane.add(Gmailjt);
+		contentpane.add(nomusuariojp);
+		contentpane.add(nomusuariojt);
+		contentpane.add(contraseniajp);
+		contentpane.add(contraseniajt);
+		contentpane.add(numtarjetajp);
+		contentpane.add(numtarjetajt);
+		contentpane.add(panelbotones);
+		contentpane.add(btnGuardar);
+		contentpane.add(btnSalir);
 		
 		
 	}
