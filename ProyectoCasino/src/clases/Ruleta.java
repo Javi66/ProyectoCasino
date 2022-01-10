b3a7281fdfbace7f822f182e33a3ecdc243a8a0f
jpomb <jpomb@DESKTOP-62VJTIV>
2022-01-10 18:07:28 +0100

    ruleta clase

--- ProyectoCasino/src/clases/Ruleta.java
+++ ProyectoCasino/src/clases/Ruleta.java
@@ -1,23 +1,21 @@
 package clases;
 
+import java.util.Random;
 
 public class Ruleta extends Juego {
 
-	private int casillas;
+	private int casillas= 37;
 	private String color;
-	private String parImpar;
 	
 	
-
 	public Ruleta() {
 		super();
 	}
 
-	public Ruleta(int casillas, String color, String parImpar) {
+	public Ruleta(int casillas, String color) {
 		super();
 		this.casillas = casillas;
 		this.color = color;
-		this.parImpar = parImpar;
 	}
 
 	public int getCasillas() {
@@ -36,20 +34,21 @@ public class Ruleta extends Juego {
 		this.color = color;
 	}
 
-	public String getParImpar() {
-		return parImpar;
-	}
-
-	public void setParImpar(String parImpar) {
-		this.parImpar = parImpar;
-	}
-
 	@Override
 	public String toString() {
-		return "Ruleta [casillas=" + casillas + ", color=" + color + ", parImpar=" + parImpar +", getGiros()=" + getGiros() + ", getDineroapostado()="
+		return "Ruleta [casillas=" + casillas + ", color=" + color + ", getCasillas()=" + getCasillas()
+				+ ", getColor()=" + getColor() + ", getGiros()=" + getGiros() + ", getDineroapostado()="
 				+ getDineroapostado() + ", getUsuario()=" + getUsuario() + ", toString()=" + super.toString()
 				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + "]";
 	}
-
+	 public int giraRuleta (){
+	        
+	        int posicion;
+	        Random random = new Random();
+	        //Calcular una posicion aleatoria de la ruleta
+	        posicion = random.nextInt(casillas);
+	        return posicion;
+	    }
 	
+
 }
